6f8f99bf837c4cca473fc463f57cbb2b594a3d65
Shaurya Gupta <shaurya50211@gmail.com>
2024-05-31 16:22:00 -0400

    Initial Commit

---
+++ Fizzix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fizzix
+//
+//  Created by Shaurya on 2024-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fizzix/FizzixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzixApp.swift
+//  Fizzix
+//
+//  Created by Shaurya on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct FizzixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
